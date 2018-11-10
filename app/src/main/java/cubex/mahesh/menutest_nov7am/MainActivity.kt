62c6d007e09310b98fbcfa7ff830416689bee8ee
maheshthippala <mahesh.cubex@gmail.com>
2018-11-10 08:12:26 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/menutest_nov7am/MainActivity.kt
@@ -0,0 +1,32 @@
+package cubex.mahesh.menutest_nov7am
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.mymenu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+     Toast.makeText(this@MainActivity,
+             item!!.title, Toast.LENGTH_LONG).show()
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+}
